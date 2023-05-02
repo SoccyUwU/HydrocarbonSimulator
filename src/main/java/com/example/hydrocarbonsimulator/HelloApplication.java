99a98db444d72866c34d86b0cb41ef5598caab29
SoccyUwU <87612938+SoccyUwU@users.noreply.github.com>
2023-05-02 17:23:55 -0400

    changed to use gridpane as base

--- src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
+++ src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
@@ -1,19 +1,17 @@
 package com.example.hydrocarbonsimulator;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application
 {
     @Override
-    public void start(Stage stage) throws IOException
+    public void start(Stage stage)
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        GridPane base = new GridPane();
+        Scene scene = new Scene(base, 320, 240);
         stage.setTitle("test tf");
         stage.setScene(scene);
         stage.show();
