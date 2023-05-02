22b20bd4f119d7ec8a38c74cad1bceab778c7f2e
SoccyUwU <87612938+SoccyUwU@users.noreply.github.com>
2023-05-02 17:14:02 -0400

    Second test

--- src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
+++ src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("test title");
+        stage.setTitle("test tf");
         stage.setScene(scene);
         stage.show();
     }
