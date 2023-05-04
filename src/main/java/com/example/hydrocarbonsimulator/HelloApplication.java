7d04e1b38bf6ee1bb6f245c7bca4aadce2f978be
SoccyUwU <87612938+SoccyUwU@users.noreply.github.com>
2023-05-04 17:49:14 -0400

    Tested font size modification and rendering text

--- src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
+++ src/main/java/com/example/hydrocarbonsimulator/HelloApplication.java
@@ -2,19 +2,32 @@ package com.example.hydrocarbonsimulator;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application
 {
+    final static int screenWidth = 320;
+    final static int screenHeight = 240;
     @Override
     public void start(Stage stage)
     {
+        Canvas lewisCanvas = new Canvas(screenWidth, screenHeight);
+        GraphicsContext lewisContext = lewisCanvas.getGraphicsContext2D();
+
         GridPane base = new GridPane();
-        Scene scene = new Scene(base, 320, 240);
+        base.add(lewisCanvas, 0, 0);
+        Scene scene = new Scene(base, screenWidth, screenHeight);
         stage.setTitle("test t");
         stage.setScene(scene);
         stage.show();
+
+        Font elementFont = new Font(20);
+        lewisContext.setFont(elementFont);
+        lewisContext.strokeText("C", 50, 40);
     }
 
     public static void main(String[] args)
